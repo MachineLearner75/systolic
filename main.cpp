1968e5c6b282a0c4cbbe13bb1591deae2df638a2
Hugo SAILHEN <b00746387@essec.edu>
2021-02-16 09:45:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+class Date {
+private:
+    int day;
+    int month;
+    int year;
+
+public:
+    Date() { }
+    Date(int d, int m, int y) {
+    day = d;
+    month = m;
+    year = y;
+  }
+
+    void printDate(){
+      std::cout << "" << day << "/";
+      std::cout << "" << month << "/";
+      std::cout << "" << year << "\n";
+  }
+};
+
+class Blood{
+  private:
+    int systolic;
+    int diastolic;
+    Date dat;
+
+  public:
+    Blood(int sys, int dia, Date dat): systolic(sys), diastolic(dia), dat(dat){
+    
+  } 
+
+  void printBlood() {
+    dat.printDate();
+
+    std::cout<<"Systolic pressure is: "<<systolic<<"\n";
+    std::cout<<"Diastolic pressure is: "<<diastolic<<"\n";
+  }
+
+  int getSystolic(){
+      return systolic;
+  }
+
+  int getDiastolic(){
+    return diastolic;
+  }
+
+};
+
+class Patient {
+  private:
+    std::string name;
+    std::vector <Blood> vect;
+
+  public:
+    Patient(std::string n){
+      name = n;
+    }
+
+    void addRecord(Blood k){
+      vect.push_back(k);
+    }
+
+    void printReport() {
+      //the highest abnormal systolic blood pressures, together with the corresponding diastolic value, and the day it has been measured;
+      int j=0;
+      double avg= 0;
+      std::cout << "Report of: "<< name << std::endl; 
+      for(int i=0;i<vect.size();i++){
+        avg+=vect[i].getDiastolic();
+        if(vect[i].getSystolic()>140){
+          std::cout<<"The systolic pressure was high this day: \n";
+          vect[i].printBlood();
+          j++;
+        }
+      }
+    avg=avg/vect.size();
+    std::cout<<"The average diastolic pressure is : " << avg <<"\n";
+    if(j==0){
+        std::cout<< "No measurement was not too high";
+    }
+  }
+};
+
+int main() {
+  Patient mary("Mary");
+    mary.addRecord(Blood(94,61, Date(2,5,2013)));
+    mary.addRecord(Blood(97,65, Date(3,5,2013)));
+    mary.addRecord(Blood(144,99, Date(4,5,2013)));
+    mary.addRecord(Blood(123,88, Date(5,5,2013)));
+    mary.addRecord(Blood(177,110, Date(6,5,2013)));
+    mary.addRecord(Blood(145,89, Date(7,5,2013)));
+    mary.printReport();
+
+    Patient john("John");
+    john.addRecord(Blood(88,57, Date(15,5,2013)));
+    john.addRecord(Blood(95,61, Date(16,5,2013)));
+    john.addRecord(Blood(114,80, Date(17,5,2013)));
+    john.addRecord(Blood(151,96, Date(18,5,2013)));
+    john.addRecord(Blood(176,104, Date(19,5,2013)));
+    john.addRecord(Blood(176,110, Date(20,5,2013)));
+    john.printReport();
+
+    Patient hugo("Hugo");
+    hugo.addRecord(Blood(88,250, Date(15,5,2013)));
+    hugo.addRecord(Blood(95,200, Date(16,5,2013)));
+    hugo.addRecord(Blood(114,80, Date(17,5,2013)));
+    hugo.addRecord(Blood(120,96, Date(18,5,2013)));
+    hugo.addRecord(Blood(105,104, Date(19,5,2013)));
+    hugo.addRecord(Blood(99,110, Date(20,5,2013)));
+    hugo.printReport();
+    return 0;
+}
